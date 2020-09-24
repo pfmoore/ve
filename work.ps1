7d7ab3b4b63fa094fdb3b2f5a345f2108dc492a8
Paul Moore <p.f.moore@gmail.com>
2020-09-24 22:40:51 +0100

    Rewrite WIP

---
+++ work.ps1
@@ -0,0 +1,28 @@
+function ve-temp {
+    [CmdletBinding(DefaultParameterSetName="ByName")]
+    Param (
+    )
+
+    # Need to expand ~
+    $name = Join-Path (Resolve-Path "~/.virtualenvs") (New-Guid)
+    virtualenv $name
+    pwsh -NoExit {
+        Write-Host -ForegroundColor Cyan "Launching nested prompt in virtual environment. Type 'exit' to return."
+        $name = $args[0]
+        echo (Join-Path $name "Scripts/activate.ps1")
+        & (Join-Path $name "Scripts/activate.ps1")
+    } -args $name
+    Remove-Item -Recurse $name
+}
+
+function ve-ls {
+    dir (Resolve-Path ~/.virtualenvs)
+}
+
+function ve {
+    $cmd = $args[0]
+    $args = $args[1..$args.Count]
+    echo $cmd
+    echo $args
+    & "ve-$cmd" @args
+}
